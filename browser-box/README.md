970913b657d191265b3769f92af81ce010fbd451
Rui Carmo <rui.carmo@gmail.com>
2013-06-10 10:02:19 +0100

    Added browser box

---
+++ browser-box/README.md
@@ -0,0 +1,12 @@
+# Browser Box
+
+A Vagrant template to quickly set up a usable browser accessible via [RDP][r].
+
+Includes:
+
+* Chromium
+* Openbox
+* xrdp
+* Microsoft Truetype fonts
+
+[r]: http://en.wikipedia.org/wiki/Remote_Desktop_Protocol
---
+++ browser-box/manifests/init.pp
@@ -0,0 +1,18 @@
+# This installes the MS TTF fonts automatically, accepting the EULA
+class msfonts {
+  exec { "accept-msttcorefonts-license":
+    command => "/bin/sh -c \"echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections\""
+  }
+
+  package { "ttf-mscorefonts-installer":
+    ensure  => installed,
+    require => Exec['accept-msttcorefonts-license']
+  }
+}
+
+include msfonts
+
+# This will also setup vnc4server as a back-end for xrdp
+package { ["chromium-browser", "openbox", "xrdp", "rxvt", "vim"]:
+    ensure => installed,
+}

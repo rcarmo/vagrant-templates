5f64512d0a82c056604bd054494435f2d4e26fa5
Rui Carmo <rui.carmo@gmail.com>
2013-06-12 22:32:06 +0100

    added a minimal panel and a way to change the GTK themes

--- browser-rdp/manifests/init.pp
+++ browser-rdp/manifests/init.pp
@@ -13,6 +13,6 @@ class msfonts {
 include msfonts
 
 # This will also setup vnc4server as a back-end for xrdp
-package { ["chromium-browser", "openbox", "xrdp", "rxvt", "vim"]:
+package { ["chromium-browser", "openbox", "xrdp", "rxvt", "vim", "fbpanel", "lxappearance"]:
     ensure => installed,
 }

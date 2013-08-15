70f867d4b37175e63e965e216ab3e6c83a0e8ed7
Rui Carmo <rui.carmo@gmail.com>
2013-08-15 17:04:17 +0000

    Added pacckages

--- browser-rdp/manifests/init.pp
+++ browser-rdp/manifests/init.pp
@@ -13,6 +13,6 @@ class msfonts {
 include msfonts
 
 # This will also setup vnc4server as a back-end for xrdp
-package { ["chromium-browser", "htop", "tmux", "openbox", "xrdp", "lxterminal", "vim", "fbpanel", "lxappearance", "bash-completion"]:
+package { ["chromium-browser", "htop", "tmux", "openbox", "xrdp", "lxterminal", "vim", "fbpanel", "lxappearance", "bash-completion","elementary-icon-theme","software-properties-common"]:
     ensure => installed,
 }
